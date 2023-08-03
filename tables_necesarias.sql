efbde5929728c48ca8a9705141a997e9fce7c6d6
DonChambitasCompany <donchambitas2003@gmail.ccom>
2023-08-02 23:31:22 -0600

    new file:   tables_necesarias.sql

---
+++ tables_necesarias.sql
@@ -0,0 +1,31 @@
+/* tabla roles*/
+INSERT INTO roles (name_rol) VALUES ('cliente'), 
+('trabajador'),
+('administrador');
+
+
+/* tabla countries*/
+INSERT INTO profesiones (name_profesion) VALUES ('Doctor'),
+('Abogado'),
+('Contador'),
+('Diseñador'),
+('Programador'),
+('Chef'),
+('Electricista'),
+('Terapeuta'),
+('Coach Personal');
+
+
+/* tabla tipo de residencia */
+INSERT INTO tipo_residencia (nombre_tipo_residencia) VALUES
+('Soy Extranjero'),
+('Soy Mexicano');
+
+
+/* tabla tipo de licencia */
+INSERT INTO tipo_licencia (nombre_tipo_licencia) VALUES
+('Tipo A'),
+('Tipo B'),
+('No aplica');
+
+
